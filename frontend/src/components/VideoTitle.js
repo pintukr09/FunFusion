19b507d14c4accb182cd1a12bffe3d6a29e8c34c
Pintu Kumar <pintukumarverma654@gmail.com>
2025-05-21 21:41:37 +0530

    text

--- frontend/src/components/VideoTitle.js
+++ frontend/src/components/VideoTitle.js
@@ -4,7 +4,7 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-[vw] absolute text-white pt-[18%] p-12">
-      <h1 className="text-3xl font-bold pt-30" > {title}</h1>
+      <h1 className="text-1xl font-bold pt-30" > {title}</h1>
    
     </div>
   );
