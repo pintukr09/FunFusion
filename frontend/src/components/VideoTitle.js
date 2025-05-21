a961a072687b2c093fb78dc9f0a55c00a12fe7d3
Pintu Kumar <pintukumarverma654@gmail.com>
2025-05-21 21:49:32 +0530

    video title font size

--- frontend/src/components/VideoTitle.js
+++ frontend/src/components/VideoTitle.js
@@ -4,8 +4,8 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-[vw] absolute text-white pt-[18%] p-12">
-      <h1 className="text-1xl font-bold pt-30" > {title}</h1>
-   
+      <h1 className="text-2xl font-bold pl-2" > {title}</h1>
+
     </div>
   );
 };
