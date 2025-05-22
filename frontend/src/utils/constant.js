9336260dcf61ffc41550102e66874c9e20e99353
Pintu Kumar <pintukumarverma654@gmail.com>
2025-05-22 13:45:30 +0530

    image url change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -8,11 +8,11 @@ export const options = {
   },
 };
 
-export const Now_Playing_Movie =  "https://api.themoviedb.org/3/movie/now_playing";
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
 export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
 export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
 export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
-export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500.jpg";
-export const SEARCH_MOVIE_URL ="https://api.themoviedb.org/3/search/movie?query=";
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/original";
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
 
 // Define it with the appropriate value
