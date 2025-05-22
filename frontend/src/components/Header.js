86936011b5548034ed726bcd9fbc4cff86ee85c7
Pintu Kumar <pintukumarverma654@gmail.com>
2025-05-22 08:46:14 +0530

    Respnsive header complete

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -33,10 +33,10 @@ const Header = () => {
     dispatch(setToggle());
   };
 
-  return (
+    return (
     <div className="absolute z-10 w-full flex flex-wrap items-center justify-between px-1 py-3 bg-gradient-to-b from-white gap-2 sm:gap-4 md:flex-row ">
       {/* Logo */}
-      <div className=" text-base sm:text-2xl font-black tracking-wide bg-gradient-to-r from-red-500 via-yellow-400 to-purple-600 text-transparent bg-clip-text animate-gradient glow-hover transition-transform  ">
+      <div className=" text-base sm:text-2xl md:text-3xl lg:text-3xl font-black tracking-wide bg-gradient-to-r from-red-500 via-yellow-400 to-purple-600 text-transparent bg-clip-text animate-gradient glow-hover transition-transform  ">
         FunFusion
       </div>
 
@@ -44,27 +44,34 @@ const Header = () => {
         <div className="flex items-center justify-end flex-wrap gap-2 sm:gap-3 max-w-full">
           {/* User Info */}
           <div className="flex items-center gap-1">
-            <VscAccount size={20} color="orange" />
-            <span className="text-sm font-medium truncate max-w-[100px] sm:max-w-[150px]">{user.fullName}</span>
+             <VscAccount
+      className="text-orange-500 
+                 w-5 h-5         // default (mobile)
+                 sm:w-6 sm:h-6   // small screens ≥ 640px
+                 md:w-7 md:h-7   // medium screens ≥ 768px
+                 lg:w-8 lg:h-8"  // large screens ≥ 1024px
+    />
+            <span className="text-sm  sm:text-sm  md:text-base  lg:text-xl font-medium truncate max-w-[100px] sm:max-w-[150px]">{user.fullName}</span>
           </div>
 
           {/* Buttons */}
           <button
             onClick={logoutHandler}
-            className="bg-red-600 text-black px-3 py-1 text-xs sm:text-sm border border-red-400 rounded hover:bg-red-700 transition"
+            className="bg-red-600 text-black px-3 py-1 text-xs sm:text-sm  md:text-base  lg:text-lg border border-red-400 rounded hover:bg-red-700 transition"
           >
             Logout
           </button>
           <button
             onClick={toggleHandler}
-            className="bg-purple-700 text-white px-3 py-1 text-xs sm:text-sm border border-purple-400 rounded hover:bg-purple-800 transition"
+            className="bg-purple-700 text-white px-3 py-1 text-xs sm:text-sm  md:text-base  lg:text-lg   border border-purple-400 rounded hover:bg-purple-800 transition"
           >
-            {toggle ? "Home" : "Search"}
+            {toggle ? "Home" : "Search Movie"}
           </button>
         </div>
       )}
     </div>
   );
+
 };
 
 export default Header;
--- frontend/src/components/VideoTitle.js
+++ frontend/src/components/VideoTitle.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-[vw] absolute text-white pt-[5%] p-8 ">
-      <h1 className="text-2xl font-bold pl-1" > {title}</h1>
+    <div>
+
 
     </div>
   );
